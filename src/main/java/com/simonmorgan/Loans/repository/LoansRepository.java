7a92f67f087218eddd3eaebd56bf19d568dffaed
Simizzle <85163420+Simizzle@users.noreply.github.com>
2025-03-27 16:10:52 +0000

    feat: add repository interface

---
+++ src/main/java/com/simonmorgan/Loans/repository/LoansRepository.java
@@ -0,0 +1,13 @@
+package com.simonmorgan.Loans.repository;
+
+import com.simonmorgan.Loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+}
