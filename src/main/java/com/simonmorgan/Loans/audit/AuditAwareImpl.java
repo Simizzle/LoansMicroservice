f0bf4ccd4ca6ca7e1d008b23ae816dd03ebc0c77
Simizzle <85163420+Simizzle@users.noreply.github.com>
2025-03-27 16:42:54 +0000

    fix: add @Component annotation to AuditAware implementation

--- src/main/java/com/simonmorgan/Loans/audit/AuditAwareImpl.java
+++ src/main/java/com/simonmorgan/Loans/audit/AuditAwareImpl.java
@@ -1,9 +1,11 @@
 package com.simonmorgan.Loans.audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class AuditAwareImpl implements AuditorAware<String> {
 
     @Override
